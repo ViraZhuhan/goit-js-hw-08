1f97e659977988d04bf6cd436c3e79a7a3be1512
ViraZhuhan <virazhuhan@gmail.com>
2023-03-30 20:24:34 +0300

    add task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay (data) {
+   localStorage.setItem(STORAGE_KEY, data.seconds)
+}
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY)).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+
 
